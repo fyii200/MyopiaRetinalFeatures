1bc9d2ee4e508366e030fa8ffe25beb49ede9838
Fabian Yii 俞思良 <72454128+fyii200@users.noreply.github.com>
2024-11-21 10:46:13 +0000

    Update utils.py

--- code/OD_segmentation/utils.py
+++ code/OD_segmentation/utils.py
@@ -1,4 +1,6 @@
 """
+Script containing a helper function that shows the predicted optic disc mask as a coloured overlay.
+
 Author : Fabian Yii
 Email  : fabian.yii@ed.ac.uk
 
@@ -9,18 +11,23 @@ import numpy as np
 import torch
 import torch.nn as nn
     
-def show_mask(mask, ax, random_color=False):
+def show_mask(mask, ax, random_color = False):
     """
     Args:
-          mask (2D ndarray)   : Predicted optic disc segmentation mask (binary).
-          ax (str)            : plt ax object showing the original image.
-          random_color (bool) : Colour of the mask overlay (random if True).
+          mask (2D ndarray)     : Predicted optic disc segmentation mask (binary).
+          ax (str)              : plt ax object showing the original image.
+          random_color (bool)   : Colour of the mask overlay (random if True).
+    
+    Return:
+          mask_image (3D array) : Fundus photograph with a coloured overlay showing the optic disc mask.
     """
+    
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
         color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
+    
+    h, w       = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)   
 
