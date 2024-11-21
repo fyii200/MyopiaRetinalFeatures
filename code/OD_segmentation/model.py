ea1690c61cc08d60776894e1a465383633212734
Fabian Yii 俞思良 <72454128+fyii200@users.noreply.github.com>
2024-11-21 10:41:55 +0000

    Update model.py

--- code/OD_segmentation/model.py
+++ code/OD_segmentation/model.py
@@ -1,4 +1,6 @@
 """
+Script defining the 'trainedModel' class, which loads a trained DeepLabV3 Mobilenet model.
+
 Author : Fabian Yii
 Email  : fabian.yii@ed.ac.uk
 
@@ -19,14 +21,17 @@ class trainedModel:
     def __init__(self, savedMaskDir, checkpoint="bestODmodel.pth"):
         """
         Args:
-              savedMaskDir (str) : Path to the folder in which the predicted masks are to be saved.
-              checkpoint (str)   : Name of the file containing the trained weights (.pth).
+              savedMaskDir (str)             : Path to the folder in which the predicted masks are to be saved.
+              checkpoint (str)               : Name of the file containing the trained weights (.pth).
+        
+        Return:    
+              predictedFinalMask (2D array)  : Predicted optic disc mask [0, 255].
         """
         
         self.savedMaskDir = savedMaskDir
-        os.makedirs(self.savedMaskDir, exist_ok=True) 
-        self.checkpoint = checkpoint
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "mps")
+        os.makedirs(self.savedMaskDir, exist_ok = True) 
+        self.checkpoint   = checkpoint
+        self.device       = torch.device("cuda" if torch.cuda.is_available() else "mps")
 
     def load(self, dir_path):
         """
@@ -36,16 +41,15 @@ class trainedModel:
         
         # Load DeepLabV3 Mobilenet and its trained weights.
         self.model = torch.hub.load('pytorch/vision:v0.10.0', 
-                                    'deeplabv3_mobilenet_v3_large',
-                                    pretrained=True)
-        self.model.classifier[4] = nn.LazyConv2d(1, 1)
+                                    'deeplabv3_mobilenet_v3_large')
+        self.model.classifier[4]     = nn.LazyConv2d(1, 1)
         self.model.aux_classifier[4] = nn.LazyConv2d(1, 1)
-        checkpoint_path = join(dir_path, self.checkpoint)
-        self.model.load_state_dict(torch.load(checkpoint_path, map_location=self.device))
+        checkpoint_path              = join(dir_path, self.checkpoint)
+        self.model.load_state_dict(torch.load(checkpoint_path, map_location = self.device))
         self.model.to(self.device)   
         self.model.eval()    
     
-    def segment(self, image, image_name, blurKernelSize=(21,21), binaryThreshold=0.95):
+    def segment(self, image, image_name, blurKernelSize = (21,21), binaryThreshold = 0.95):
         """
         Args:
               image (4D ndarray)              : Input image of shape [B,C,H,W].
@@ -58,23 +62,23 @@ class trainedModel:
         """
         
         with torch.no_grad():
-            outputs = self.model(image.to(self.device))['out'] # (B, 1, H, W)
-            output = outputs[0,0,:,:] # (H, W)
-            output = cv.GaussianBlur(output.detach().cpu().numpy(), blurKernelSize, 0)
-            output = torch.sigmoid(torch.tensor(output)).detach().cpu().numpy()
+            outputs             = self.model(image.to(self.device))['out'] # (B, 1, H, W)
+            output              = outputs[0,0,:,:] # (H, W)
+            output              = cv.GaussianBlur(output.detach().cpu().numpy(), blurKernelSize, 0)
+            output              = torch.sigmoid(torch.tensor(output)).detach().cpu().numpy()
             predictedBinaryMask = np.where(output >= binaryThreshold, 1, 0)
         
         ########################################### Quality control ###########################################
         #  If there's more than 1 predicted mask, select the mask that most probably corresponds to the optic #
-        #  disc. This is done by selecting the most circular shape. If there's still more than one after this #
-        #                             step, select the mask with the largest area.                            #
+        #  disc. This is done by selecting the most circular shape. If there's still more than one predicted  #
+        #                     mask after this step, select the mask with the largest area.                    #
         ####################################################################################################### 
         cnts, hierarchy = cv.findContours(np.uint8(predictedBinaryMask), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         if len(cnts)>1:
             contour_list = []
             for contour in cnts:
                 approx = cv.approxPolyDP(contour, 0.01*cv.arcLength(contour, True), True)
-                area = cv.contourArea(contour)
+                area   = cv.contourArea(contour)
                 if ((len(approx)>8) & (area > 30)): # only add circular contour to the list
                     contour_list.append(contour)
             if len(contour_list) != 0:
@@ -85,8 +89,7 @@ class trainedModel:
         else:
             predictedFinalMask = cv.drawContours(np.zeros(image[0,0,:,:].shape), cnts, -1, 1, -1)
    
-        # Save predicted mask. Note: cv.imwrite only accepts [0,255] pixel values;
-        # otherwise, image will be all black.
+        # Save predicted mask
         cv.imwrite(join(self.savedMaskDir, image_name), predictedFinalMask*255)
         
         # # Save original image with the mask overlay.
