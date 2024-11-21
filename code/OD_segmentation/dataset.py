747f16debcdb0b9f0a6ad851f295c5b4afb00b19
Fabian Yii 俞思良 <72454128+fyii200@users.noreply.github.com>
2024-11-21 10:31:52 +0000

    Update dataset.py

--- code/OD_segmentation/dataset.py
+++ code/OD_segmentation/dataset.py
@@ -1,4 +1,6 @@
 """
+Script defining the 'DatasetGenerator' class for getting and preprocessing fundus images on the UK Biobank Research Analysis Platform.
+
 Author : Fabian Yii
 Email  : fabian.yii@ed.ac.uk
 
@@ -24,8 +26,8 @@ class DatasetGenerator(Dataset):
         """
 
         self.image_names = list(data_frame.fundus)
-        self.eyes = list(data_frame.eye)
-        self.crop = CenterCrop(croppedSize)
+        self.eyes        = list(data_frame.eye)
+        self.crop        = CenterCrop(croppedSize)
 
     def __getitem__(self, index):
         """
@@ -35,7 +37,7 @@ class DatasetGenerator(Dataset):
         """
             
         image_name = self.image_names[index]
-        eye = self.eyes[index]
+        eye        = self.eyes[index]
         
         if eye == 'RE':
             data_dir = join('/mnt/project/Bulk/Retinal Optical Coherence Tomography', 'Fundus (right)', str(image_name[0:2]) )
@@ -45,17 +47,17 @@ class DatasetGenerator(Dataset):
         image = cv.imread(join(data_dir, image_name))
         
         # Centre crop to "croppedSize"
-        image = self.crop(torch.tensor(image).moveaxis(2,0)).moveaxis(0,2).detach().numpy() 
+        image        = self.crop(torch.tensor(image).moveaxis(2,0)).moveaxis(0,2).detach().numpy() 
         # Convert to RGB
-        image = cv.cvtColor(image, cv.COLOR_BGR2RGB).astype(np.float32) 
+        image        = cv.cvtColor(image, cv.COLOR_BGR2RGB).astype(np.float32) 
         # Resize to 560 by 560 pixels
-        resizeWidth = int(image.shape[1] * 40 / 100)  # scale down by 40%
+        resizeWidth  = int(image.shape[1] * 40 / 100)  # scale down by 40%
         resizeHeight = int(image.shape[0] * 40 / 100) # scale down by 40%
-        resizeDim = (resizeWidth, resizeHeight)
-        image = cv.resize(image, resizeDim, interpolation = cv.INTER_AREA)
+        resizeDim    = (resizeWidth, resizeHeight)
+        image        = cv.resize(image, resizeDim, interpolation = cv.INTER_AREA)
         # normalise to [0-1] range and reshape into [C,H,W]  
-        image = torch.tensor(image)
-        image = image.moveaxis(2,0) / torch.max(image) 
+        image        = torch.tensor(image)
+        image        = image.moveaxis(2,0) / torch.max(image) 
         
         return image, image_name
 
